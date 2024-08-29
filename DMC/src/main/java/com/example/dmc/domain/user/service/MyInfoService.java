3ac01bf87a9a0443d82083e2e8f07172b0f03b29
박주원 <qkrwndnjs7308@gmail.com>
2024-08-30 01:52:06 +0900

    MyInfoService

---
+++ DMC/src/main/java/com/example/dmc/domain/user/service/MyInfoService.java
@@ -0,0 +1,11 @@
+package com.example.spring.domain.user.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MyInfoService {
+
+
+}
