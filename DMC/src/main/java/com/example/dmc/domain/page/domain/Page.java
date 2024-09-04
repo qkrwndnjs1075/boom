f4f0599aaddc0731a2c17e8a9a505384a1d28db3
박주원 <qkrwndnjs7308@gmail.com>
2024-09-04 14:59:18 +0900

    Page

---
+++ DMC/src/main/java/com/example/dmc/domain/page/domain/Page.java
@@ -0,0 +1,35 @@
+package com.example.dmc.domain.page.domain;
+
+import com.example.dmc.domain.user.domain.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Page {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String pageId;
+
+    private String title;
+
+    private String content;
+
+    private String author;
+
+    @Column(name = "club_image_url",columnDefinition = "TEXT")
+    private String pageImageUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+
+
+
+
+}
