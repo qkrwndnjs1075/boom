31825fe793eaae49aa31c3fab03c84bc5427db11
박주원 <qkrwndnjs7308@gmail.com>
2024-09-06 19:16:27 +0900

    r

--- DMC/src/main/java/com/example/dmc/domain/page/domain/repository/PageRepository.java
+++ DMC/src/main/java/com/example/dmc/domain/page/domain/repository/PageRepository.java
@@ -4,7 +4,7 @@ import com.example.dmc.domain.page.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface PageRepository extends JpaRepository<Page,String> {
-    Page findByPageId(String pageId);
+public interface PageRepository extends JpaRepository<Page,Long> {
+    Page findByPageId(Long pageId);
 
 }
