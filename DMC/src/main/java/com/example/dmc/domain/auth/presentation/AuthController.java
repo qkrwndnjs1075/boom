6d408a86253d5d71d5134eb04a666bde3a7dbe64
박주원 <qkrwndnjs7308@gmail.com>
2024-09-06 19:16:10 +0900

    수정

--- DMC/src/main/java/com/example/dmc/domain/auth/presentation/AuthController.java
+++ DMC/src/main/java/com/example/dmc/domain/auth/presentation/AuthController.java
@@ -1,24 +1,27 @@
 package com.example.dmc.domain.auth.presentation;
 
 import com.example.dmc.domain.auth.presentation.dto.request.LoginRequest;
+import com.example.dmc.domain.auth.presentation.dto.request.RefreshTokenRequest;
 import com.example.dmc.domain.auth.presentation.dto.request.RegisterRequest;
 import com.example.dmc.domain.auth.presentation.dto.response.LoginResponse;
 import com.example.dmc.domain.auth.service.LoginService;
+import com.example.dmc.domain.auth.service.LogoutService;
 import com.example.dmc.domain.auth.service.RegisterService;
+import com.example.dmc.domain.auth.service.ReissueService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/atuh")
+@RequestMapping("/auth")
 public class AuthController {
 
     private final RegisterService registerService;
     private final LoginService loginService;
+    private final LogoutService logoutService;
+    private final ReissueService reissueService;
 
     @PostMapping("/register")
     public void register(@RequestBody @Valid RegisterRequest request){
@@ -29,6 +32,17 @@ public class AuthController {
     public LoginResponse login(@RequestBody @Valid LoginRequest request){
         return loginService.login(request);
     }
+
+    @PostMapping("/logout")
+    public void logout(@RequestParam String accessToken){
+        logoutService.logout(accessToken);
+    }
+
+    @PatchMapping("/reissue")
+    public LoginResponse reissue(@RequestBody @Valid RefreshTokenRequest request){
+        return reissueService.reissue(request);
+    }
+
 
 
 
