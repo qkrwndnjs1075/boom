e2d695f11c97860f1b6903a9e5ddb8e919acd05c
박주원 <qkrwndnjs7308@gmail.com>
2024-09-06 19:15:58 +0900

    수정

--- DMC/src/main/java/com/example/dmc/domain/auth/domain/repository/RefreshTokenRepository.java
+++ DMC/src/main/java/com/example/dmc/domain/auth/domain/repository/RefreshTokenRepository.java
@@ -8,4 +8,6 @@ import java.util.Optional;
 
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken,String> {
     Optional<RefreshToken> findByToken(String token);
+
+    void deleteByToken(String accountId);
 }
