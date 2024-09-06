a23d22217b977e8e9c7957cddcda16ed56a33f09
박주원 <qkrwndnjs7308@gmail.com>
2024-09-06 19:16:29 +0900

    r

--- DMC/src/main/java/com/example/dmc/domain/user/domain/repository/UserRepository.java
+++ DMC/src/main/java/com/example/dmc/domain/user/domain/repository/UserRepository.java
@@ -5,8 +5,9 @@ import com.example.dmc.domain.user.domain.User;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
+import java.util.UUID;
 
-public interface UserRepository extends CrudRepository<User,Long> {
+public interface UserRepository extends CrudRepository<User, UUID> {
 
     Optional<User> findByAccountId(String accountId);
 
